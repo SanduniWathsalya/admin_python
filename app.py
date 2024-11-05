57202025f2daadc677bba940af37ddbe04c16368
SanduniWathsalya <sanduniwathsalya0620@gmail.com>
2024-11-05 23:53:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,26 @@
 from flask import Flask, send_file
+from flask_cors import CORS  # Import CORS
 import matplotlib.pyplot as plt
 from io import BytesIO
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for the entire app
 
 @app.route('/chart2')
 def generate_chart():
-    # Data for the chart
     labels = ['Sent mail to registered', 'Paid for register']
     sizes = [120, 85]
     colors = ['#140C73', '#3A96C9']
-
-
-
-    # Create the plot
     plt.style.use('ggplot')
     plt.title("Students' activation")
-    plt.pie(x=sizes,labels=labels,autopct='%.2f%%',startangle=90,colors=colors)
+    plt.pie(sizes, labels=labels, autopct='%.2f%%', startangle=90, colors=colors)
     plt.axis('equal')
-    plt.legend(loc='upper right')
-
-    circle = plt.Circle(xy=(0,0),radius=.75, facecolor='white')
+    circle = plt.Circle((0, 0), 0.75, color='white')
     plt.gca().add_artist(circle)
-    plt.show()
-
-
-    # Save the plot to a BytesIO object
     img = BytesIO()
     plt.savefig(img, format='png', transparent=True)
     img.seek(0)
     plt.close()
-
     return send_file(img, mimetype='image/png')
 
 if __name__ == '__main__':
